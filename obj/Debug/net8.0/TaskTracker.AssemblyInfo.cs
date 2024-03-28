a8c038750ab024931883121232d09f98e7820437
Peiling <jxg9@txstate.edu>
2024-03-28 13:01:07 -0500

    add Migration

--- obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskTracker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25baeaf911067881d444e76600bc9df253cb417e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b557f25e347c2a8948fae2ff8672694a077a7d37")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
